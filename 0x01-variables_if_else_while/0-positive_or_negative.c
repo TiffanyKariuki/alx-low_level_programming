9cef7e87f1066861b31d54517db9637e2e8cb053
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-02 15:35:22 -0700

    Positive anything is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 nclude <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  *main -main block
  *Description:Get a random number and print the number 
@@ -8,16 +9,16 @@ nclude <stdlib.h>
  * *Return:0
  */
 int main(void)
-{
-                int n;
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-	        if (n >0
-		printf("%i is positive\n",n);
-	        else if(n < 0)
-	        printf("%i is negative\n",n);     
-		else
-	        printf("%i is zero\n",n);						
-		return (0);
+{    
+int n;
 
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n >0)
+	printf("%i is positive\n",n);
+else if(n < 0)
+        printf("%i is negative\n",n);     
+else
+        printf("%i is zero\n",n);						
+return (0);
 }
