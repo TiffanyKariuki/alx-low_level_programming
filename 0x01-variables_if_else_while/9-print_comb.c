f68d6f6727af7f10e78b9d0b95e7ef7b702226b7
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-03 02:59:44 -0700

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,20 @@
 #include < stdio.h >
-
 /**
- *main - Entry point
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return : Always 0 (success)
  */
-
 int main(void)
-
 {
-	int k;
-
-	for (k = 48; k <= 57; k++)
+	int k
+	for (k=48;, k <=57; k++)
 	{
-		putchar(k);
-		if (k < 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+	putchar(k);
+if(k < 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
