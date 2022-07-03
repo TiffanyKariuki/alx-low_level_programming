9888228c473044c76b99d2f61caa9f8db7a8b54f
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-03 12:10:34 -0700

    success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -20,16 +20,16 @@ int main(void)
 		putchar(n);
 
 		if (n != 57)
-		
-	        {
 
-															putchar(',');
+		{
 
-															putchar(' ');
+																	putchar(',');
 
-															}
+																				putchar(' ');
 
-	       {
+																						}
+
+		}
 
 		putchar('\n');
 
