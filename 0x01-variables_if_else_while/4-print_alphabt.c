3c079a09b84f528c22167d7aaeb760b7b10508ad
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-04 11:16:38 -0700

    win

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 
-
 /**
  * main - prints the alphabet in lowercase,
  * followed by a new line, except q and e
@@ -26,9 +25,10 @@ int main(void)
 
 															putchar(ch);
 
-															}
-                                
-	        ch++;
+		
+		}
+		
+		ch++;
 
 		}
 
