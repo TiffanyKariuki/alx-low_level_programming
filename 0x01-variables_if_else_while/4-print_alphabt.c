4453c7e3939b42d71422e7dfdcb1a39149f8268d
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-04 11:26:03 -0700

    win

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,7 @@
 
 int main(void)
 
-{
+
 
 		char ch = 'a';
 
@@ -17,16 +17,16 @@ int main(void)
 
 		while (ch <= 'z')
 
-		{
+		
 
 		if (ch != 'e' && ch != 'q')
 
+		
 		{
 
-															putchar(ch);
-
 		
-		}
+		putchar(ch);
+		
 		
 		ch++;
 
