b0e7d3549ded8d3ef55a5c89c36b4267a8de95d2
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-05 16:35:10 -0700

    2print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,39 @@
 #include "main.h"
 
+
+
 /**
- *add -> adds two number
- *@x : param1
- *@y : param2
- *Return: x + y
+ *
+ * * print_alphabet_x10 -> prints the lowercase alphabets
+ *
  * */
 
 
 
-int add(int x, int y)
+void print_alphabet_x10(void)
 
 {
 
-	return (x + y);
+		int j;
+
+		int x;
+
+
+
+		for (x = 0; x < 10; x++)
+
+		{
+
+		for (j = 'a'; j <= 'z'; j++)
+
+												{
+
+			  									_putchar(j);
+
+												}
+
+												_putchar('\n');
+
+												}
 
 }
