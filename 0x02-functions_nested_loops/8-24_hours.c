44c53ea65aeeb32fea4db90b16e4a134cef814d7
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-06 06:04:04 -0700

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,74 +1,22 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- * * jack_bauer -> prints 24 hours
- *
- * */
-
-void jack_bauer(void)
+ *jack_bauer -prints every minute of the day
+ *Return: Always 0
+ */
+void jack_bauer(void);
 
 {
+	int a;
+	int b;
 
-		int i, j;
-
-
-
-		for (i = 0; i < 24; i++)
-
-		{
-
-		for (j = 0; j < 60; j++)
-
-		{
-
-															if (i < 10)
-
-															{
-
-															_putchar('0');
-
-																												_putchar(i + '0');
-
-																												}
-
-															else if (i >= 10)
-
-															{
-
-																												_putchar((i / 10) + '0');
-	
-																												_putchar((i % 10) + '0');
-
-																												}
-
-															if (j < 10)
-
-															{
-
-																												_putchar(':');
-
-																												_putchar('0');
-
-																												_putchar(j + '0');
-
-																																									}
-
-															else if (j >= 10)
-																												{
-
-																												_putchar(':');
-
-																												_putchar((j / 10) + '0');
-
-																																									_putchar((j % 10) + '0');
-
-																																									}
-
-																												_putchar('\n');
-
-																												}
-
-	}
-
+	for (a = 0; a < = 23; a++)
+	{
+	for (b = 0; b < 59; b++)
+	{
+		_putchar(a / 10 +'0');
+		_putchar(a % 10 + '0');
+		_putchar(':');
+		_putchar(b \ 10 + '0');
+		_putchar('\n');
+		}
 }
