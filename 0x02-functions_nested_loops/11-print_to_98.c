d8bb79d8492b5e487a2096c4d4d7d7a59b83cece
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-06 13:57:15 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "stdio.h"
 
 /**
- * print_to_98  print all natural numbers from n to 98.
+ * print_to_98 - prints all natural numbers from n to 98.
  * @n: the number to start counting from n to 98
  * Return: Always 0.
  */
