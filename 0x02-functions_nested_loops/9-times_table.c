6a087e03860d36ec2978230293820d5d086e02da
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-06 16:14:38 -0700

    learn

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -29,19 +29,19 @@ void times_table(void)
 				_putchar(' ');
 				_putchar('0' + rep);
 			}
+			
 			else
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
 		}
 		if (b < 9)
 		{
-			_putchar(',')
-			_putchar(' ')
+			_putchar(',');
+			_putchar(' ');
 		}
 		b++;
 	}
 	_putchar('\n');
 	a++;
 }
-
 }
