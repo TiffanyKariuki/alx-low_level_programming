e85150f4d1db58a45fcb4b8a218fc1d1b6c6b892
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-11 15:23:10 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,65 +1,40 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * puts_half - void
- *
- *   *
- *
- *    * @str: string
- *
- *     *
- *
- *      */
+ * puts_half -void
+ * @str: string
+ */
 
 void puts_half(char *str)
 
 {
+	int len, n, i;
 
-		int len, n, i;
+	len = 0;
+	while (str[len] != '\0')
 
-
-
-		len = 0;
-
-
-
-		while (str[len] != '\0')
-
-		{
-
+	{
 		len++;
 
+	}
+	if (len % 2 == 0)
+
+	{
+		for  (i = len / 2; str[i] != '\0'; i++)
+
+		{
+			_putchar(str[i]);
 		}
-
-
-
-		if (len % 2 == 0)
+	}
+		else if (len % 2)
 
 		{
+			for (n = (len - 1) / 2; n < len - 1; n++)
 
-		for (i = len / 2; str[i] != '\0'; i++)
+			{
+				_putchar(str[n + 1]);
 
-		{
-															_putchar(str[i]);
-															}
-
-											} else if (len % 2)
-
-													{
-
-																for (n = (len - 1) / 2; n < len - 1; n++)
-
-																			{
-
-																							_putchar(str[n + 1]);
-
-																									}
-
-																	}
-
-												_putchar('\n');
-
+			}
+			_putchar('\n');
+		}
 }
