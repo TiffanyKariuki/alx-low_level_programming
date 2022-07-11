74842416eac3edaa9d22817ccd068660d99e2b44
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-11 16:30:37 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * puts2 - print one char out of 2 of a string
+ * puts_half - print one char out of 2 of a string
  * @str: string to print
  * Return: Nothing
  */
 
-void puts2(char *str)
+void puts_half(char *str)
 
 {
 
