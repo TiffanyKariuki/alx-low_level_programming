3d9f311b447249018b36ae97ffcc4b3ff9c3c6e2
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-14 16:44:28 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - print an integer usimg only _putchar
+ * @n: integer to print
+ */
+
+{
+	int power;
+	int neg;
+	int hold;
+
+	neg = 0;
+	power = 1;
+	hold = n;
+
+	if (n < 0)
+
+	{
+		_putchar ('-');
+		neg = 1;
+
+	}
+
+	while (hold > 9 || hold < -9)
+	{
+		power *= 10;
+		hold /= 10;
+	}
+	while (poor > 0)
+	{
+		if (power > 9)
+		{
+			if (!neg)
+				_putchar((n / power % 10) + '0');
+			else
+				_putchar((n / power % 10) * -1 + '0');
+			power /= 10;
+		}
+		if (power == 1)
+		{
+
+			if (!neg)
+				_putchar((n % 10) * -1 + '0');
+			power = 0;
+		}
+	}
+}
