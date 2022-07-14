fd763d64f64bb1a2969a792c81b243a97bd5207b
TiffanyKariuki <tiffanykariuki17@gmail.com>
2022-07-13 18:54:10 -0700

    string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,77 +1,50 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * cap_string - capitalize all words of a string
- *
- *   * @str: string
- *
- *    * Return: `str`
- *
- *     */
-
-
+ *cap_string - capitalize all words of a string
+ * @str: string
+ * Return: 'str'
+ */
 
 char *cap_string(char *str)
-
 {
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t\" ";
 
-		int i, c;
+	for (i = 0, trigger = 0; str[i] != '\0'; i++)
 
-		int trigger;
+	{
+	if (str[0] > 96 && str[0] < 123)
 
-		char nots[] = ",;.!?(){}\n\t\" ";
+		trigger = 1;
+	for (c = 0; nots[c] != '\0'; c++)
 
+	{
+		if (nots[c] == str[i])
 
+			trigger = 1;
+	}
 
-		for (i = 0, trigger = 0; str[i] != '\0'; i++)
+	if (trigger)
+
+	{
+		if (str[i] > 96 && str[i] < 123)
 
 		{
+			str[i] -= 32;
 
-		if (str[0] > 96 && str[0] < 123)
+			trigger = 0;
+		}
 
-														trigger = 1;
+		else if (str[i] > 64 && str[i] < 91)
+			trigger = 0;
+		else if (str[i] > 47 && str[i] < 58)
 
-												for (c = 0; nots[c] != '\0'; c++)
+			trigger = 0;
+		}
 
-															{
+	}
 
-																			if (nots[c] == str[i])
-
-																			trigger = 1;
-
-																					}
-
-
-
-														if (trigger)
-
-																	{
-
-																	if (str[i] > 96 && str[i] < 123)
-
-																	{
-
-																														str[i] -= 32;
-
-																																		trigger = 0;
-
-																																					}
-
-																	else if (str[i] > 64 && str[i] < 91)
-
-																													trigger = 0;
-
-																											else if (str[i] > 47 && str[i] < 58)
-
-																																trigger = 0;
-
-																													}
-
-															}
-
-						return (str);
-
+	return (str);
 }
